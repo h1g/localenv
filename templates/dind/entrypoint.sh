81e8de0bd254d06e683f5aef39e9a855200be265
Геннадий Новиков <6142843+h1g@users.noreply.github.com>
2020-07-25 18:01:04 +0300

    optimize dind/entrypoint.sh template

--- templates/dind/entrypoint.sh
+++ templates/dind/entrypoint.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 cleanup() {
-[ -f "/tmp/eventlistner.pid" ] && kill `cat /tmp/eventlistner.pid`
+[ -f "/tmp/eventlistner.pid" ] && kill $(cat /tmp/eventlistner.pid)
 sed '/#Localenv hosts records/,$d' /etc/hosts.docker > /tmp/hosts.docker && cat /tmp/hosts.docker > /etc/hosts.docker
 }
 
 cleanup
 
 format="{{.NetworkSettings.Networks.localenv.IPAddress}} {{ join .NetworkSettings.Networks.localenv.Aliases \" \"}}"
-[ $(cat /etc/hosts|grep host.docker.internal|wc -l) -eq 0 ] && format="127.0.0.1 {{ join .NetworkSettings.Networks.localenv.Aliases \" \"}}"
+[ "$(grep -c host.docker.internal /etc/hosts)" -eq 0 ] && format="127.0.0.1 {{ join .NetworkSettings.Networks.localenv.Aliases \" \"}}"
 
-echo "#Localenv hosts records" >> /etc/hosts.docker
-cat /etc/hosts|grep host.docker.internal >> /etc/hosts.docker
-docker inspect --format "${format}" $(docker ps --format {{.ID}})|awk '{if (NF>1 && $1!="<no") print $0}' >> /etc/hosts.docker
+{ echo "#Localenv hosts records"; grep -w host.docker.internal /etc/hosts; docker inspect --format "${format}" $(docker ps --format {{.ID}})|awk '{if (NF>1 && $1!="<no") print $0}'; } >> /etc/hosts.docker
 
-docker events --filter 'event=start' --filter 'event=stop' --format '{{.Status}} {{.ID}}'|while read line;
+docker events --filter 'event=start' --filter 'event=stop' --format '{{.Status}} {{.ID}} {{.Actor.Attributes.name}}'|while read -r -a info;
 do
-  sed "/$(docker inspect --format '{{.Name}}' $(echo $line|cut -d' ' -f2)|sed 's/\///')/d" /etc/hosts.docker > /tmp/hosts.docker && cat /tmp/hosts.docker > /etc/hosts.docker
-  [ "$(echo $line|cut -d' ' -f1)" == "start" ] && docker inspect --format "${format}" $(echo $line|cut -d' ' -f2)|awk '{if (NF>1 && $1!="<no") print $0}' >> /etc/hosts.docker
+  if [ "${info[0]}" == "start" ];then
+    docker inspect --format "${format}" ${info[1]}|awk '{if (NF>1 && $1!="<no") print $0}' >> /etc/hosts.docker
+  else
+    sed "/${info[2]}/d" /etc/hosts.docker > /tmp/hosts.docker && cat /tmp/hosts.docker > /etc/hosts.docker
+  fi
 done &
-
 echo $! > /tmp/eventlistner.pid
 
 trap cleanup SIGINT SIGTERM
